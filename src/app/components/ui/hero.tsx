6be86f24de6feeee69dd968680af20ffac3ac434
Hifza-Code-Journey <hifza.she.codes@gmail.com>
2024-04-25 20:30:52 +0500

    first commit

---
+++ src/app/components/ui/hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='flex flex-col gap-7 md:flex-row min-h-[300px] bg-green-500 lg:bg-blue-500 sm:bg-purple-500 items-center px-4 justify-between'>
+      <div className='flex flex-col items-start gap-2'>
+      <h1 className='font-bold text-3xl'>Welcome</h1>
+      <p className='w-96'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, ullam. Magni quisquam vel sint magnam error assumenda optio voluptatem nam officia iste et facilis, dolore non tenetur nulla minima iure.</p>
+      </div>
+      <button>
+        
+      </button>
+    </div>
+  )
+}
+
+export default Hero
